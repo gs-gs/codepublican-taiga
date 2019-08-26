3291330e4790c2b5497d0a8e1736b11c09d024fc
roleyfoley <roleyfoley@gmail.com>
2019-08-27 01:03:55 +1000

    configure celery

--- taiga-back/src/settings/celery_local.py
+++ taiga-back/src/settings/celery_local.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from kombu import Queue
 from .overrides.base import *
 from .overrides.kms import env
 
