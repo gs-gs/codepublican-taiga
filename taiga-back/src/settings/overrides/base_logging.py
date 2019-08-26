254df0126414642ffd5b979064b43f09ca1135ac
roleyfoley <roleyfoley@gmail.com>
2019-08-26 22:05:49 +1000

    add sentry logging

--- taiga-back/src/settings/overrides/base_logging.py
+++ taiga-back/src/settings/overrides/base_logging.py
@@ -5,73 +5,35 @@ SENTRY_DSN = env('SENTRY_DSN', default=None)
 SENTRY_CLIENT = env('DJANGO_SENTRY_CLIENT', default='raven.contrib.django.raven_compat.DjangoClient')
 
 if SENTRY_DSN:
-    SENTRY_CELERY_LOGLEVEL = env.int('DJANGO_SENTRY_LOG_LEVEL', logging.INFO)
     RAVEN_CONFIG = {
         'dsn': SENTRY_DSN,
-        'release': env('BUILD_REFERENCE', default='unknown'),
+        'release': env('SENTRY_RELEASE', default='unknown'),
+        'environment': env('SENTRY_ENVIRONMENT', default='unkown'),
     }
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(message)s'
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
-        },
-        'sentry': {
-            'level': 'WARNING',
-            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
-        },
-    },
-    'loggers': {
-        '': {
-            'handlers': ['console', 'sentry'],
-            'level': 'DEBUG',
-        },
-        'elasticsearch': {
-            'handlers': ['console', 'sentry'],
-            'level': 'WARNING',
-        },
-        'sentry': {
-            'handlers': ['console'],
-            'propagate': False,
-            'level': 'WARNING',
-        },
-        'raven': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'faker': {  # boring stuff from the tests
-            'level': 'INFO',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'factory': {  # boring stuff from the tests
-            'level': 'INFO',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'sentry.errors': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        'django.security.DisallowedHost': {
-            # boring to have it in sentry
-            'handlers': ['console'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-    },
+LOGGING['handlers']['console']['level'] = 'INFO'
+LOGGING['handlers']['sentry'] = { 
+    'level' : 'WARNING',
+    'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+}
+
+LOGGING['loggers']["sentry"] = {
+    'handlers': ['console'],
+    'propagate': False,
+    'level': 'WARNING',
+}
+
+LOGGING['loggers']['raven'] = {
+    'level': 'DEBUG',
+    'handlers': ['console'],
+    'propagate': False,
+}
+
+LOGGING['loggers']['sentry.errors'] = {
+    'level': 'DEBUG',
+    'handlers': ['console'],
+    'propagate': False,
 }
 
 # remove sentry for non-sentrified installations
