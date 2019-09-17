4bbc3b71a688e6cf7e58a6c0a347e4c4ada5a04d
roleyfoley <roleyfoley@gmail.com>
2019-09-17 15:59:27 +1000

    override root directories for storage

--- taiga-back/src/settings/overrides/production.py
+++ taiga-back/src/settings/overrides/production.py
@@ -96,5 +96,9 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/"
-STATIC_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/static/"
+
+STATIC_ROOT = 'static'
+STATIC_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{STATIC_ROOT}/"
+
+MEDIA_ROOT = 'media'
+MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{MEDIA_ROOT}/"
